b62ddf3e25780fe4ca41e7edd12bfa5891277f82
Gueyepa1 <papa-modou.gueye@outlook.fr>
2022-06-09 17:04:56 +0200

    test de commit à partir d'écllipse

---
+++ src/main/java/fr/isika/cda18/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.isika.cda18;
+
+/**
+ * @author papamodougueye
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
